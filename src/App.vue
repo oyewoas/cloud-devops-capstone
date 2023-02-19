2edd1dcc85e6a41daa3eb8dc95fc676e67e92ad9
oyewoas <ayooluwaoyewo@gmail.com>
2023-02-19 18:03:15 +0100

    Added Dockerfile to other files

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+
+    <NavBar/>
+    <CoreView/>
+    <CoreFooter/>
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .app{
+    font-family: 'Roboto', sans-serif;
+
+  }
+</style>
+
+
+<script>
+import NavBar from '@/components/common/NavBar'
+import CoreView from '@/components/common/View'
+import CoreFooter from '@/components/common/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    CoreView,
+    CoreFooter,
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
